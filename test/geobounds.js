203d33d282cd240ebb2a191e31e2139078a1fab7
Damian Krzeminski <pirxpilot@furkot.com>
2025-05-01 21:17:56 -0600

    transition to ESM format

--- test/geobounds.js
+++ test/geobounds.js
@@ -1,5 +1,5 @@
-const { describe, it } = require('node:test');
-const bounds = require('..');
+import { describe, it } from 'node:test';
+import bounds from '../lib/geobounds.js';
 
 describe('bounds', () => {
   it('should fail on empty', t => {
